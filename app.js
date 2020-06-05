8785b0f3ae421b8c7d51fdcfc2aaba9425e4d995
Michael Vondung <michael.vondung@gmail.com>
2020-06-05 11:52:17 +0200

    Changed from save() to create(), added redirect

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Basic task inventory manager
 // Starting date: 2020-05-28 12:40:18
 // Based on Get Programming with Node.JS book
-// Updated: 2020-06-03 18:01:20
+// Updated: 2020-06-05 11:50:43
 
-// Todo: Use create for saving new tasks (p.211)
-// Todo: Redirect to task overview after saving (p.211)
+// Done: Todo: Use create for saving new tasks (p.211)
+// Done: Redirect to task overview after saving (p.211)
 // Todo: Wrap tasks into a link with _id so they can be edited/updated later (l. 19.6)
 
 const express = require('express'),
@@ -13,8 +13,8 @@ const express = require('express'),
     mongoose = require('mongoose'),
     homeController = require('./controllers/home-controller'),
     errorController = require('./controllers/error-controller'),
-    tasksController = require('./controllers/tasks-controller')
-    router = express.Router();
+    tasksController = require('./controllers/tasks-controller');
+router = express.Router();
 
 mongoose.connect('mongodb://localhost:27017/task_inventory', {
     useNewUrlParser: true,
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(homeController.logIncomingRequestsToConsole);
-app.use('/', router)
+app.use('/', router);
 
 // routes
 router.get('/', homeController.showIndexPage);
@@ -46,7 +46,7 @@ router.get('/', homeController.showIndexPage);
 // database routes
 router.get('/tasks', tasksController.showAllTasks);
 router.get('/add-task', tasksController.showNewTaskForm);
-router.post('/save-task', tasksController.saveNewTaskToDatabase);
+router.post('/save-task', tasksController.saveNewTaskToDatabase, tasksController.redirectView);
 
 router.get('/delete-all', tasksController.deleteAllTask);
 
--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -22,64 +22,31 @@ module.exports = {
     showNewTaskForm: (req, res) => {
         res.render('new-task');
     },
-    // ! The following function should probably be rewritten to use Task.Create with a redirect.
-    saveNewTaskToDatabase: (req, res) => {
-        let newTask = new Task({
+    saveNewTaskToDatabase: (req, res, next) => {
+        Task.create({
             name: req.body.name,
             priority: req.body.priority,
-        });
-        newTask
-            .save()
+        })
             .then((result) => {
-                res.render('task-added');
+                console.log(result);
             })
             .catch((error) => {
-                res.send(error);
+                console.error(error.message);
+                res.send(error.message);
             });
+        next();
     },
     deleteAllTask: (req, res) => {
         Task.remove({})
-        .exec()
-        .then(() => {
-            console.log('All tasks were deleted from the database')
-        })
-        .catch(error => {
-            console.log(`Error deleting all tasks: ${error.message}`)
-        })
-    }
+            .exec()
+            .then(() => {
+                console.log('All tasks were deleted from the database');
+            })
+            .catch((error) => {
+                console.log(`Error deleting all tasks: ${error.message}`);
+            });
+    },
+    redirectView: (req, res) => {
+        res.redirect('tasks');
+    },
 };
-
-// exports.showAllTasks = (req, res) => {
-//     Task.find({})
-
-//         .exec()
-//         .then((tasks) => {
-//             res.render('tasks', { tasks: tasks });
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//             return [];
-//         })
-//         .then(() => {
-//             console.log('from task-controller.js: showAllTasks completed');
-//         });
-// };
-
-// exports.showNewTaskForm = (req, res) => {
-//     res.render('new-task');
-// };
-
-// exports.saveNewTaskToDatabase = (req, res) => {
-//     let newTask = new Task({
-//         name: req.body.name,
-//         importance: req.body.importance,
-//     });
-//     newTask
-//         .save()
-//         .then((result) => {
-//             res.render('task-added');
-//         })
-//         .catch((error) => {
-//             res.send(error);
-//         });
-// };
