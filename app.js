d05efe0becda46cb32e09051ce9aba52e624110a
Michael Vondung <michael.vondung@gmail.com>
2020-06-08 16:20:50 +0200

    new dependency, new routes

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
 // Todo: Implement http-override module.
 // Todo: Considering moving task-related views into a tasks folder.
 // Todo: Add res.locals.redirect to tasks-controller for flexible redirects.
+// Todo: Add editing/updating and deleting of records.
+// Todo: Document the code better.
 
 const express = require('express'),
     app = express(),
@@ -17,12 +19,14 @@ const express = require('express'),
     homeController = require('./controllers/home-controller'),
     errorController = require('./controllers/error-controller'),
     tasksController = require('./controllers/tasks-controller'),
+    methodOverride = require('method-override'),
     router = express.Router();
 
 mongoose.connect('mongodb://localhost:27017/task_inventory', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true
+    // useCreateIndex: true,
+    useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -40,6 +44,7 @@ router.use(layouts);
 router.use(express.static('public'));
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json());
+router.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 router.use(homeController.logIncomingRequestsToConsole);
 
@@ -51,6 +56,9 @@ router.get('/tasks', tasksController.showAllTasks);
 router.get('/add-task', tasksController.showNewTaskForm);
 router.post('/save-task', tasksController.saveNewTaskToDatabase, tasksController.redirectView);
 router.get('/tasks/:id', tasksController.showDetails, tasksController.showDetailsView);
+router.get('/tasks/:id/edit', tasksController.editDetails);
+router.put('/tasks/:id/update', tasksController.updateRecord, tasksController.redirectView);
+router.delete('/tasks/:id/delete', tasksController.deleteRecord, tasksController.redirectView);
 
 router.get('/delete-all', tasksController.deleteAllTask);
 
