80c6750bd3066da7019394b6b8ceb7904920e40d
Michael Vondung <michael.vondung@gmail.com>
2020-06-25 15:02:52 +0200

    moved request logging to routes/routes.js

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,22 +1,9 @@
-// This file handles has general controllers
-
 // 2020-06-03 10:57:14 - Updated to a style where one object with functions is exported.
+// 2020-06-25 14:11:30 - Moved request logger to routes.js.
 
 module.exports = {
-    logIncomingRequestsToConsole: (req, res, next) => {
-        console.log(req.url);
-        next();
-    },
     showIndexPage: (req, res) => {
         res.render('index');
     },
 };
 
-// exports.logIncomingRequestsToConsole = (req, res, next) => {
-//     console.log(req.url);
-//     next();
-// };
-
-// exports.showIndexPage = (req, res) => {
-//     res.render('index');
-// };
